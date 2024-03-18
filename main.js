7066ba8c742df8c679811284c22eed223c3b40f2
Zain Tanveer <76905685+Zain-Tanveer@users.noreply.github.com>
2024-03-18 09:38:24 +0500

    fixed hamburger-close icon

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
+const hamburgerIcon = document.querySelector(".icon-hamburger");
+const closeIcon = document.querySelector(".icon-close");
+
 navToggle.addEventListener("click", (e) => {
   primaryNav.hasAttribute("data-visible")
     ? navToggle.setAttribute("aria-expanded", false)
@@ -9,6 +12,14 @@ navToggle.addEventListener("click", (e) => {
 
   primaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
+
+  if (primaryNav.hasAttribute("data-visible")) {
+    hamburgerIcon.style.display = "none";
+    closeIcon.style.display = "initial";
+  } else {
+    hamburgerIcon.style.display = "initial";
+    closeIcon.style.display = "none";
+  }
 
   document.body.style.overflow =
     document.body.style.overflow == "hidden" ? "auto" : "hidden";
